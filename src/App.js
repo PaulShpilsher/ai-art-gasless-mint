d86f58aaea46ba456c52879ae02230d26e20cab5
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-03-12 15:22:28 +0300

    add textbox and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+
+  console.log(prompt);
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-extrabold">
-        AI NTF Mint
-      </h1>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1 className="text-4xl font-extrabold">AI Art Gasless mints</h1>
+      {/* Create an input box and button saying next beside it */}
+      <div className="flex items-center justify-center gap-4">
+        <input
+          className="border-2 border-black rounded-md p-2"
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Enter a prompt"
+        />
+        <button className="bg-black text-white rounded-md p-2">Next</button>
+      </div>
     </div>
   );
 }
