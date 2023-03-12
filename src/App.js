05128a0a1a07cfa304ae17b75eadea8897d031b5
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-03-12 15:07:13 +0300

    change label

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-4xl font-extrabold">
-        Todo List
+        AI NTF Mint
       </h1>
     </div>
   );
