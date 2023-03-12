a49a4c48a7940f46266faa2d5b2a2647f3580db1
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-03-12 17:19:58 +0300

    add prompts for image name, descr, and wallet

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ const convertUriIfpsToHttps = (url) => {
 
 function App() {
   const [prompt, setPrompt] = useState("");
-  const [imageBlob, setImageBlob] = useState(null);
   const [file, setFile] = useState(null);
+  const [imageBlob, setImageBlob] = useState(null);
+
+  const [imageName, setImageName] = useState("");
+  const [imageDescription, setImageDescription] = useState("");
+
+  const [walletAddress, setWalletAddress] = useState("0xb8Dfff92aa9A06A0CfF733D6cd1B62a560B622ef");
 
   const generateArt = async () => {
     try {
@@ -48,8 +53,8 @@ function App() {
       });
 
       const store = await nftstorage.store({
-        name: `AI NFT: "${prompt.toLowerCase()}"`,
-        description: `AI generated NFT of "${prompt.toLowerCase()}"`,
+        name: imageName,
+        description: imageDescription,
         image: file,
       });
 
@@ -64,21 +69,21 @@ function App() {
   const mintNft = async () => {
     try {
       const imageURL = await uploadArtToIpfs();
-  
+
       // mint as an NFT on nftport
       const response = await axios.post(
         `https://api.nftport.xyz/v0/mints/easy/urls`,
         {
           file_url: imageURL,
           chain: "polygon",
-          name: `AI NFT: "${prompt.toLowerCase()}"`,
-          description: `AI generated NFT of "${prompt.toLowerCase()}"`,
-          mint_to_address: "0xb8Dfff92aa9A06A0CfF733D6cd1B62a560B622ef",
+          name: imageName ?? `AI ART: "${prompt.trim().toLowerCase()}`,
+          description: imageDescription ?? "",
+          mint_to_address: walletAddress,
         },
         {
           headers: {
             Authorization: process.env.REACT_APP_NFT_PORT,
-          }
+          },
         }
       );
       const data = await response.data;
@@ -88,17 +93,15 @@ function App() {
     }
   };
 
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-4xl font-extrabold">AI Art Gasless mints</h1>
-      {/* Create an input box and button saying next beside it */}
       <div className="flex items-center justify-center gap-4">
         <input
           className="border-2 border-black rounded-md p-2"
           onChange={(e) => setPrompt(e.target.value)}
           type="text"
-          placeholder="Enter a prompt"
+          placeholder="What do you wanna see"
         />
         <button
           onClick={generateArt}
@@ -111,11 +114,30 @@ function App() {
         {imageBlob && (
           <div className="flex flex-col gap-4 items-center justify-center">
             <img src={imageBlob} alt={`AI generated art of "${prompt}"`} />
+            <input
+              className="border-2 border-black rounded-md p-2"
+              onChange={(e) => setImageName(e.target.value)}
+              type="text"
+              placeholder="Enter name"
+            />
+            <input
+              className="border-2 border-black rounded-md p-2"
+              onChange={(e) => setImageDescription(e.target.value)}
+              type="text"
+              placeholder="Enter description"
+            />
+            <input
+              className="border-2 border-black rounded-md p-2"
+              onChange={(e) => setWalletAddress(e.target.value)}
+              type="text"
+              placeholder="Mint to address"
+              value={walletAddress}
+            />            
             <button
               onClick={mintNft}
               className="bg-black text-white rounded-md p-2"
             >
-              Upload to IPFS
+              Mint NFT
             </button>
           </div>
         )}
